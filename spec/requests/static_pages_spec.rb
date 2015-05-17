0a8947ef7fc190a0456fa5d72bf549b57d36a50e
kubosho <ta2@o2p.jp>
2015-05-18 06:56:47 +0900

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,12 +7,34 @@ RSpec.describe "StaticPages", type: :request do
         visit '/static_pages/home'
         expect(page).to have_content('Sample App')
       end
+
+      it "should have the right title" do
+        visit '/static_pages/home'
+        expect(page).to have_title("クソリプブロッカー | Home")
+      end
     end
 
     describe "Help page" do
       it "should have the content 'Help'" do
         visit '/static_pages/help'
         expect(page).to have_content('Help')
+      end
+
+      it "should have the right title" do
+        visit '/static_pages/help'
+        expect(page).to have_title("クソリプブロッカー | Help")
+      end
+    end
+
+    describe "About page" do
+      it "should have the content 'About us'" do
+        visit '/static_pages/about'
+        expect(page).to have_content('About us')
+      end
+
+      it "should have the right title" do
+        visit '/static_pages/about'
+        expect(page).to have_title("クソリプブロッカー | About")
       end
     end
   end
