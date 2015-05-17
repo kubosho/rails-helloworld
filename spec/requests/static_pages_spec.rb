38d1a52bf617cfb6d16b734a8b5ffdf7362acb14
kubosho <ta2@o2p.jp>
2015-05-18 01:29:42 +0900

    First test (use RSpec)

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+  describe "GET /static_pages" do
+    describe "Home page" do
+      it "should have the content 'Sample App'" do
+        visit '/static_pages/home'
+        expect(page).to have_content('Sample App')
+      end
+    end
+
+    describe "Help page" do
+      it "should have the content 'Help'" do
+        visit '/static_pages/help'
+        expect(page).to have_content('Help')
+      end
+    end
+  end
+end
