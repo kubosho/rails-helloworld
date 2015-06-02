f972a112d082ebf98b2a2e7f6c97689979e777f5
kubosho <ta2@o2p.jp>
2015-06-02 09:33:55 +0900

    Add test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,37 +1,62 @@
 require 'rails_helper'
 
 describe 'StaticPages', type: :request do
-  let(:base_title) { 'クソリプブロッカー' }
   subject { page }
 
-  describe 'GET /static_pages' do
+  shared_examples_for 'all static pages' do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  describe 'Home page' do
     before { visit root_path }
 
-    describe 'Home page' do
-      it { should have_title("#{base_title}") }
-      it { should have_content('Sample App') }
-      it { should_not have_title('| Home') }
-    end
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
 
-    describe 'Help page' do
-      before { visit help_path }
+    it_should_behave_like 'all static pages'
+    it { should_not have_title('| Home') }
+  end
 
-      it { should have_content('Help') }
-      it { should have_title("#{base_title} | Help") }
-    end
+  describe 'Help page' do
+    before { visit help_path }
 
-    describe 'About page' do
-      before { visit about_path }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
 
-      it { should have_content('About us') }
-      it { should have_title("#{base_title} | About") }
-    end
+    it_should_behave_like 'all static pages'
+  end
 
-    describe 'Contact page' do
-      before { visit contact_path }
+  describe 'About page' do
+    before { visit about_path }
 
-      it { should have_content('Contact') }
-      it { should have_title("#{base_title} | Contact") }
-    end
+    let(:heading) { 'About us' }
+    let(:page_title) { 'About' }
+
+    it_should_behave_like 'all static pages'
+  end
+
+  describe 'Contact page' do
+    before { visit contact_path }
+
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+
+    it_should_behave_like 'all static pages'
+  end
+
+  it 'should have the right links on the layout' do
+    visit root_path
+    click_link 'About'
+    expect(page).to have_title(full_title('About'))
+    click_link 'Help'
+    expect(page).to have_title(full_title('Help'))
+    click_link 'Contact'
+    expect(page).to have_title(full_title('Contact'))
+    click_link 'Home'
+    click_link 'Sign up now!'
+    expect(page).to have_title(full_title('Sign up'))
+    click_link 'sample app'
+    expect(page).to have_title(full_title(''))
   end
 end
