f185e7e6f566283bb6176123cb17b0dc826a346d
kubosho <ta2@o2p.jp>
2015-06-09 22:48:20 +0900

    Add email address with mixed case test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -93,4 +93,14 @@ describe User, type: :model do
       specify { expect(user_for_invalid_password).to be_falsey }
     end
   end
+
+  describe 'email address with mixed case' do
+    let(:mixed_case_email) { 'Foo@ExaMPLe.COm' }
+
+    it 'should be saved as all lower-case' do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
 end
